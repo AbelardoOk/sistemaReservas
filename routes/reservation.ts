ed174bbd56ece85e0fddd2fc4dcc67fe4250c377
AbelardoOk <abelardoemailprivado@gmail.com>
2025-04-14 08:50:42 -0400

    Feat: Verificação de OVERLAP de horarios e validade de datas

--- routes/reservation.ts
+++ routes/reservation.ts
@@ -18,15 +18,37 @@ route.post(
     endTime.setHours(endTime.getHours() + 2);
 
     try {
-      const newReservation = await await pool.query(
-        "INSERT INTO reservations (user_id, room_id, start_time, end_time) VALUES ($1, $2, $3, $4)",
-        [userId, roomId, startTime, endTime]
+      const overlap = await pool.query(
+        `SELECT * FROM reservations
+        WHERE room_id = $1
+        AND ($2::timestamp, $3::timestamp) OVERLAPS (start_time, end_time)`,
+        [roomId, startTime, endTime]
       );
 
-      res.status(201).send({
-        mensagem: "Agendamento criado com sucesso!",
-        info: newReservation,
-      });
+      const now = new Date();
+      now.setHours(now.getHours() - 4);
+      const diff = (startTime.getTime() - now.getTime()) / (1000 * 60 * 60);
+
+      if (overlap.rows.length > 0) {
+        res.status(400).send({
+          erro: "Já existe uma reserva nesta sala e horário.",
+        });
+      } else if (diff <= 0) {
+        res.status(400).send({
+          erro: "Data inválida!",
+        });
+      } else {
+        const newReservation = await pool.query(
+          `INSERT INTO reservations (user_id, room_id, start_time, end_time) 
+           VALUES ($1, $2, $3, $4) RETURNING *`,
+          [userId, roomId, startTime, endTime]
+        );
+
+        res.status(201).send({
+          mensagem: "Agendamento criado com sucesso!",
+          info: newReservation.rows[0],
+        });
+      }
     } catch (err) {
       console.error(err);
       res.status(500).json({ erro: `Erro ao criar reserva: ${err}` });
